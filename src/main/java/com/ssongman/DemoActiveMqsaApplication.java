8667824cd5bd00fa4dd42925b0fbe0fc4bbaa7ec
ssongman <ssongmantop@gamil.com>
2019-03-11 10:33:19 +0900

    first commit

---
+++ src/main/java/com/ssongman/DemoActiveMqsaApplication.java
@@ -0,0 +1,14 @@
+package com.ssongman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoActiveMqsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoActiveMqsaApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/ssongman/config/Config.java
@@ -0,0 +1,35 @@
+package com.ssongman.config;
+
+import javax.jms.Queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
+
+@Configuration
+public class Config {
+	
+	@Value("${activemq.broker-url}")
+	private String brokerURL;
+	
+	@Bean
+	public Queue queue() {
+		return new ActiveMQQueue("standalone.queue");
+	}
+	
+	@Bean
+	public ActiveMQConnectionFactory activeMQConnectionFactory () {
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+		factory.setBrokerURL(brokerURL);
+		return factory;
+	}
+	
+	@Bean
+	public JmsTemplate jmsTemplate () {
+		return new JmsTemplate(activeMQConnectionFactory());
+	}
+
+}
